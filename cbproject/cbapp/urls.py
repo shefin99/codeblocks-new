e1074cc8fce080558a5dc3fcf24fd4cc24f3999a
shefin <shefinmathew2612@gmail.com>
2023-05-18 20:28:37 +0530

    codeblocks project second commit

--- cbproject/cbapp/urls.py
+++ cbproject/cbapp/urls.py
@@ -3,10 +3,8 @@ from . import views
 
 urlpatterns=[
     path('',views.home,name='home'),
-  
-    path('login',views.login_user,name='login'),
     path('signup',views.signup,name='signup'),
-
-
-     
+    path('login',views.login_user,name='login'),
+    path('logout',views.logout_user,name='logout'),
+         
 ]
--- cbproject/cbapp/views.py
+++ cbproject/cbapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from . models import Domain
 from django.contrib import messages
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate,login,logout
 from .forms import createUserForm
 # Create your views here.
 
@@ -10,7 +10,6 @@ def home(request):
     domain = Domain.objects.all()
     context={'domain':domain}
     return render(request,"cbapp/home.html",context)
-
 
 
 def signup(request):
@@ -24,11 +23,12 @@ def signup(request):
     context = {'form':form}
     return render(request,"cbapp/signup.html",context)
 
+
 def login_user(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-
+        
         user = authenticate(request,username=username,password=password)
 
         if user is not None:
@@ -41,5 +41,8 @@ def login_user(request):
     
     return render(request,"cbapp/login.html")
 
+def logout_user(request):
+    logout(request)
+    return redirect('/')
 
 
